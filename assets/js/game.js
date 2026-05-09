2aa3cbaba46d1e759b498e34131cd5be8e7855a2
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-05-08 22:56:39 -0700

    setup random word

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,16 @@
 const keyboard = document.querySelector('#keyboard');
+const secret = document.querySelector('#secret');
 
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const secretWords = ["COMPUTER", "SMART PHONE", "CODING", "MEGABITE", "APPLICATION", ];
+
+function randomSecretWord(){
+    const index = Math.floor(Math.random() * secretWords.length);
+    word = secretWords[index];
+
+    secret.innerHTML = `<h2>${word}</h2>`;
+}
 
 function displayLetters() {
     for (let i = 0; i < alphabet.length; i++) {
@@ -11,3 +21,4 @@ function displayLetters() {
 };
 
 displayLetters();
+randomSecretWord();
